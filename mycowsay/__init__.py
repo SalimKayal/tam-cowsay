1d825861c2acfa496e8a7b98a753783925cb73f3
Salim Kayal <salim.kayal@idiap.ch>
2025-04-07 16:41:04 +0200

    feat: verbosity

--- mycowsay/__init__.py
+++ mycowsay/__init__.py
@@ -2,5 +2,6 @@ import cowsay
 import sys
 
 def say_python():
-    text = f"{sys.version} \r Modules in : [{', '.join(sys.path)}]"
+    with open('/etc/os-release', 'r') as os:
+        text = f"OS: {os.read()}\n\rPython: {sys.version}\n\rModules in : [{', '.join(sys.path)}]"
     print(cowsay.cowsay(text, width=80))
